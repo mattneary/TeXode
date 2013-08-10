af1f9bd79a09fd8c6faabc12d67311219800862e
Matt Neary <neary.matt@gmail.com>
2013-08-10 16:11:56 -0400

    Persistent config and ruby script run from warehouse

--- src/texode.sh
+++ src/texode.sh
@@ -19,7 +19,8 @@ elif [ $1 == "-i" ]
 then
   :>$TEXODE_WAREHOUSE_DIR/input
   vi $TEXODE_WAREHOUSE_DIR/input
-  cat $TEXODE_WAREHOUSE_DIR/input | ruby $TEXODE_WAREHOUSE_DIR/filter.rb
+  cd $TEXODE_WAREHOUSE_DIR
+  cat $TEXODE_WAREHOUSE_DIR/input | ruby filter.rb
 elif [ $1 == "-u" ]  
 then
   index=0
@@ -28,7 +29,8 @@ then
     if [ $index -gt 0 ]
     then
       echo "Rendering the contents of \`$arg\`."
-      ruby $TEXODE_WAREHOUSE_DIR/filter.rb  < $arg > $TEXODE_WAREHOUSE_DIR/tmp
+      cd $TEXODE_WAREHOUSE_DIR
+      ruby filter.rb  < $arg > $TEXODE_WAREHOUSE_DIR/tmp
       cat $TEXODE_WAREHOUSE_DIR/tmp > $arg      
     fi
     ((index++))
@@ -42,7 +44,8 @@ then
     if [ $index -gt 0 ] && [ $arg != $build_dir ]
     then
       echo "Rendering the contents of \`$arg\` to the folder \`$build_dir\`."
-      ruby $TEXODE_WAREHOUSE_DIR/filter.rb  < $arg > $TEXODE_WAREHOUSE_DIR/tmp
+      cd $TEXODE_WAREHOUSE_DIR
+      ruby filter.rb  < $arg > $TEXODE_WAREHOUSE_DIR/tmp
       cat $TEXODE_WAREHOUSE_DIR/tmp > $build_dir/$(basename $arg)
     fi
     ((index++))
@@ -50,7 +53,8 @@ then
 elif [ $1 == "-o" ]
 then
   echo "Rendering contents of \`$2\` to the file \`$3\`."
-  ruby $TEXODE_WAREHOUSE_DIR/filter.rb  < $2 > $TEXODE_WAREHOUSE_DIR/tmp
+  cd $TEXODE_WAREHOUSE_DIR
+  ruby filter.rb  < $2 > $TEXODE_WAREHOUSE_DIR/tmp
   cat $TEXODE_WAREHOUSE_DIR/tmp > $3
 elif [ $1 == "-v" ]
 then
@@ -76,7 +80,13 @@ then
   echo "  https://github.com/mattneary/TeXode/commit/`cat $TEXODE_WAREHOUSE_DIR/version.txt`."
 elif [ $1 == "--config" ]
 then
+  if [ -f $TEXODE_WAREHOUSE_DIR/config.json ]
+  then
   vi $TEXODE_WAREHOUSE_DIR/config.json
+  else
+  cp vi $TEXODE_WAREHOUSE_DIR/_config.json vi $TEXODE_WAREHOUSE_DIR/config.json
+  vi $TEXODE_WAREHOUSE_DIR/config.json
+  fi  
 else
   echo "Invalid arguments provided. Run \`texode\` for help."
 fi
